8621f6ee851dbdf7e0bd289e965d39b6bc7bddd3
david@cleverpine.com <david@cleverpine.com>
2023-06-19 08:59:53 +0300

    added logstash JSON Template layout, and releasing 1.0.7 version

--- src/main/java/com/cleverpine/springlogginglibrary/util/Constants.java
+++ src/main/java/com/cleverpine/springlogginglibrary/util/Constants.java
@@ -9,4 +9,23 @@ public class Constants {
     public static final String STDOUT_APPENDER_NAME = "Stdout";
     public static final String LOGSTASH_APPENDER_NAME = "Logstash";
     public static final int RECONNECT_DELAY = 5000;
+
+    public static final String JSON_TEMPLATE_LOGSTASH = """
+            {
+              "@timestamp": "${json:timeMillis}",
+              "@version": "1",
+              "message": "${json:message}",
+              "logger_name": "${json:loggerName}",
+              "thread_name": "${json:threadName}",
+              "level": "${json:level}",
+              "level_value": "${json:levelValue}",
+              "stack_trace": "${json:thrown:stackTrace:text}",
+              "#if($${json:contextMap:isEmpty})": {
+                "#drop": {}
+              },
+              "#else": {
+                "${json:contextMap}": {}
+              }
+            }
+            """;
 }
